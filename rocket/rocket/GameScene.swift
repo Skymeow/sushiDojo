9bda1cbe86f2629f5294a38a52e96831b3d7acfe
Sky Xu <skyrocks88xmz@gmail.com>
2018-03-03 23:02:58 -0800

    finish reset rocket

--- rocket/rocket/GameScene.swift
+++ rocket/rocket/GameScene.swift
@@ -15,7 +15,8 @@ class GameScene: SKScene {
     var angle: CGFloat = CGFloat(30/360 * Double.pi)
     var vel: CGFloat = 5
     var rotationV: CGFloat = 5
-    
+    var width: CGFloat?
+    var height: CGFloat?
     // Make Rocket Sub class
     // Reset the rocket after it leaves the screen
     // make target show up at a random position
@@ -23,19 +24,10 @@ class GameScene: SKScene {
     // Keep score
     
     override func didMove(to view: SKView) {
+        width = self.view?.frame.width
+        height = self.view?.frame.height
         makeRocket()
-        
     }
-//    create rocket
-    func makeRocket() {
-        let size = CGSize(width: 50, height: 50)
-        let texture = SKTexture(imageNamed: "box")
-        rocket = Rocket(texture: texture, color: UIColor.blue, size: size)
-        addChild(rocket)
-        rocket.position.x = self.size.width / 2
-        rocket.position.y = self.size.height / 5
-    }
-    
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let firstTouch = touches.first else { return }
@@ -46,7 +38,9 @@ class GameScene: SKScene {
     
 
     override func update(_ currentTime: TimeInterval) {
-        angle += rotationV/360 * CGFloat(Double.pi) //
+        //  MARK: reset the rocket back to a new start once it leaves the screen
+        resetRocket()
+        angle += rotationV/360 * CGFloat(Double.pi)
         
         let dy = sin(angle) * vel
         let dx = cos(angle) * vel
@@ -54,6 +48,32 @@ class GameScene: SKScene {
         rocket.position.x += dx
         
         rocket.zRotation = angle
-        // print(sin(angle), cos(angle))
+    }
+    
+    //    create rocket
+    func makeRocket() {
+        let size = CGSize(width: 50, height: 50)
+        let texture = SKTexture(imageNamed: "box")
+        rocket = Rocket(texture: texture, color: UIColor.blue, size: size)
+        addChild(rocket)
+        rocket.position.x = self.size.width / 3
+        rocket.position.y = self.size.height / 8
+    }
+    
+    func randomizeStart() -> CGPoint {
+        let x = Int(arc4random_uniform(UInt32(self.width!)))
+        let y = Int(arc4random_uniform(UInt32(self.height!)))
+        let randomPoint = CGPoint(x: x, y: y)
+        
+        return randomPoint
+    }
+    
+//    reset rocket function
+    func resetRocket() {
+        if rocket.position.x < 0 || rocket.position.y < 0 || rocket.position.x > width! || rocket.position.y > height! {
+            
+//            let newPoint = self.randomizeStart()
+//            self.rocket.position = newPoint
+        }
     }
 }
